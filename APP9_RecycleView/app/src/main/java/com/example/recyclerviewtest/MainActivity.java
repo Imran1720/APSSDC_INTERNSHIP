165c5f505d7cf3863bb7815a2243383ef9b8cd1c
IMRAN <skimran1720@gmail.com>
2021-06-08 19:02:32 +0530

    updated APP9_RecycleView

--- APP9_RecycleView/app/src/main/java/com/example/recyclerviewtest/MainActivity.java
+++ APP9_RecycleView/app/src/main/java/com/example/recyclerviewtest/MainActivity.java
@@ -5,9 +5,14 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements itemAdapter.MyInterface {
     RecyclerView rv;
+    int totalamount=0;
+    Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,10 +20,24 @@ public class MainActivity extends AppCompatActivity {
 
         int[] images={R.drawable.a,R.drawable.b,R.drawable.c,R.drawable.d,R.drawable.e,R.drawable.f,R.drawable.g,R.drawable.h,R.drawable.i,R.drawable.j};
         String[] itemNames={"Chicken Biryani","Chicken Lolipop","Egg fried Rice","Chicken 65","Mutton Curry","Mutton Biryani","Fish curry","Prawns curry","Veg Noodels","Chicken Noodels"};
-        String[] itemprices={"200","250","100","350","400","350","250","250","100","150"};
+        String[] price={"200","250","100","350","400","350","250","250","100","150"};
         rv=findViewById(R.id.recyclerview);
+        button=findViewById(R.id.button);
+
         rv.setLayoutManager(new LinearLayoutManager(this));
 
+        itemAdapter adapter=new itemAdapter(this,images,itemNames,price,this);
+        rv.setAdapter(adapter);
 
+    }
+
+    public void submit(View view) {
+    }
+
+    @Override
+    public void selectedItems(String itemname, String itemprice, int position, int totalprice) {
+        totalamount+=Integer.parseInt(itemprice);
+        Toast.makeText(this, ""+totalprice, Toast.LENGTH_SHORT).show();
+        button.setText(String.valueOf(totalamount));
     }
 }
--- APP9_RecycleView/app/src/main/java/com/example/recyclerviewtest/itemAdapter.java
+++ APP9_RecycleView/app/src/main/java/com/example/recyclerviewtest/itemAdapter.java
@@ -1,33 +1,91 @@
 package com.example.recyclerviewtest;
 
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.cepheuen.elegantnumberbutton.view.ElegantNumberButton;
+
 public class itemAdapter extends RecyclerView.Adapter<itemAdapter.MyViewHolder> {
+
+    Context ct;
+    int[] myImages;
+    String[] myNames,myPrices;
+    MyInterface myinterface;
+
+    public itemAdapter(MainActivity mainActivity, int[] images, String[] itemNames, String[] price,MyInterface myinterface) {
+        ct = mainActivity;
+        myImages = images;
+        myNames = itemNames;
+        myPrices = price;
+        this.myinterface = myinterface;
+    }
+
+    public interface MyInterface{
+        void selectedItems(String itemname,String itemprice,int position,int totalprice);
+
+    }
 
     @NonNull
     @org.jetbrains.annotations.NotNull
     @Override
     public itemAdapter.MyViewHolder onCreateViewHolder(@NonNull @org.jetbrains.annotations.NotNull ViewGroup parent, int viewType) {
-        return null;
+        View v= LayoutInflater.from(ct).inflate(R.layout.item_row,parent,false);
+        MyViewHolder holder=new MyViewHolder(v);
+        return holder;
     }
 
     @Override
     public void onBindViewHolder(@NonNull @org.jetbrains.annotations.NotNull itemAdapter.MyViewHolder holder, int position) {
-
+        holder.iv.setImageResource(myImages[position]);
+        holder.tv1.setText(myNames[position]);
+        holder.tv2.setText(myPrices[position]);
+//        holder.button.setOnClickListener(new ElegantNumberButton.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                String num = holder.button.getNumber();
+//                Toast.makeText(ct, ""+num, Toast.LENGTH_SHORT).show();
+//            }
+//        });
+        holder.button.setOnValueChangeListener(new ElegantNumberButton.OnValueChangeListener() {
+            @Override
+            public void onValueChange(ElegantNumberButton view, int oldValue, int newValue) {
+                Log.i("sss",String.valueOf(oldValue));
+                Log.i("sss",String.valueOf(newValue));
+               // Toast.makeText(ct, ""+newValue+" "+oldValue, Toast.LENGTH_SHORT).show();
+                String itemName = holder.tv1.getText().toString();
+                int itemprice = Integer.parseInt(holder.tv2.getText().toString());
+                int totalprice = itemprice*newValue;
+                myinterface.selectedItems(itemName,String.valueOf(itemprice),position,totalprice);
+            }
+        });
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return myImages.length;
     }
 
     public class MyViewHolder extends RecyclerView.ViewHolder {
+        ImageView iv;
+        TextView tv1,tv2;
+        ElegantNumberButton  button;
         public MyViewHolder(@NonNull @org.jetbrains.annotations.NotNull View itemView) {
+
             super(itemView);
+
+            iv = itemView.findViewById(R.id.itemimage);
+            tv1=itemView.findViewById(R.id.itemname);
+            tv2=itemView.findViewById(R.id.itemprice);
+            button=itemView.findViewById(R.id.number_button);
         }
     }
 }
