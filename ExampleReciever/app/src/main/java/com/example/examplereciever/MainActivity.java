08c09442d238ce82692b92469f3dcf0329c2b3e9
IMRAN <skimran1720@gmail.com>
2021-06-10 18:45:25 +0530

    ExampleMusic & ExampleReciever added

---
+++ ExampleReciever/app/src/main/java/com/example/examplereciever/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.examplereciever;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+    ImageView iv;
+    MyReceiver reciever;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        iv=findViewById(R.id.iv);
+
+        reciever = new MyReceiver(iv);
+
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(Intent.ACTION_POWER_CONNECTED);
+        filter.addAction(Intent.ACTION_POWER_DISCONNECTED);
+        this.registerReceiver(reciever,filter);
+    }
+}
