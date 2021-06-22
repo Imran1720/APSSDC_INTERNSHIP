12a4a25a71f774195625050eea83300d19e4709d
IMRAN <skimran1720@gmail.com>
2021-06-22 18:30:23 +0530

    updated firebase example

--- FirebaseExample/app/src/main/java/com/example/firebaseexample/MainActivity.java
+++ FirebaseExample/app/src/main/java/com/example/firebaseexample/MainActivity.java
@@ -1,16 +1,22 @@
 package com.example.firebaseexample;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
@@ -26,11 +32,23 @@ public class MainActivity extends AppCompatActivity {
         mail = findViewById(R.id.email);
         pass = findViewById(R.id.password);
         auth = FirebaseAuth.getInstance();
+        if(!isConnected()){
+            Toast.makeText(this, "Turn on Internet First", Toast.LENGTH_SHORT).show();
+        }
+        if(auth.getCurrentUser()!=null)
+        {
+            startActivity(new Intent(this,HomeActivity.class));
+            finish();
+        }
     }
 
     public void login(View view) {
         String email = mail.getText().toString().trim();
         String password = pass.getText().toString();
+        if (!isConnected()){
+            Toast.makeText(this, "Turn on internet First"
+                    , Toast.LENGTH_SHORT).show();
+        }
         if(email.isEmpty()|password.isEmpty())
         {
             Toast.makeText(this, "Fill valid username/password", Toast.LENGTH_SHORT).show();
@@ -41,6 +59,7 @@ public class MainActivity extends AppCompatActivity {
                 public void onComplete(@NonNull Task<AuthResult> task) {
                     if(task.isSuccessful()){
                         startActivity(new Intent(MainActivity.this,HomeActivity.class));
+
                     }
                     else{
                         Toast.makeText(MainActivity.this, "Authentication Failed!", Toast.LENGTH_SHORT).show();
@@ -48,5 +67,65 @@ public class MainActivity extends AppCompatActivity {
                 }
             });
         }
+    }
+
+    public void register(View view) {
+        startActivity(new Intent(this,RegisterActivity.class));
+    }
+
+    public void reset(View view) {
+        AlertDialog.Builder b = new AlertDialog.Builder(this);
+        View v= LayoutInflater.from(this).inflate(R.layout.reset,null,false);
+        EditText email = v.findViewById(R.id.editemailAddress);
+        b.setView(v);
+        b.setCancelable(false);
+        b.setPositiveButton("Reset", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                String e= email.getText().toString();
+                if(e.isEmpty()){
+                    email.setError("Can't be empty!");
+                }
+                else{
+                    auth.sendPasswordResetEmail(e).addOnCompleteListener(MainActivity.this, new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if(task.isSuccessful())
+                            {
+                                Toast.makeText(MainActivity.this, "Resent link sent", Toast.LENGTH_SHORT).show();
+                            }
+                            else {
+                                Toast.makeText(MainActivity.this, "Invalid email", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+                }
+            }
+        });
+
+        b.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+        b.show();
+    }
+    public boolean isConnected()
+    {
+        boolean conneted=false;
+        try {
+
+
+            ConnectivityManager manager = (ConnectivityManager) getSystemService(CONNECTIVITY_SERVICE);
+            NetworkInfo info = manager.getActiveNetworkInfo();
+            conneted = info != null && (info.isAvailable()) && (info.isConnected());
+            return conneted;
+        }
+        catch (Exception e)
+        {
+
+        }
+        return conneted;
     }
 }
